d7a4af3eafc1a944803a0de57b1a377c0cf21fb2
ht <thinhtruongdhth19@gmail.com>
2024-08-15 13:33:26 +0700

    Frist Commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Hello TruongTheHaiThinh");
+        System.out.println("Welcome to the DHHTTT19");
+    }
+}
